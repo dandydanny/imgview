54b20c4fccb233f652d389fd20903f34bb28458a
Daniel Lu <dandydanny@gmail.com>
2014-10-26 18:06:32 -0700

    Initial commit with ideas

---
+++ imgview.js
@@ -0,0 +1,61 @@
+// Steps
+// -----
+
+// Look inside div with class "md"
+
+// Underneath, loop inside p tags
+
+// If it's an image link
+// 	insert img src tag
+
+
+// Do this for all image links.
+
+
+// Updated Steps
+// -------------
+
+// Better method, won't ruin surprise:
+
+// On mouse over img link, display image
+
+
+
+
+var imageUrl;
+
+$( "a" ).mouseover(function() {
+	var imageUrl = $(this).attr('href');
+	console.log(imageUrl);
+}).html( "<img src=&quot;imageUrl&quot;>" );
+
+
+// ---
+
+
+$( "a" ).mouseover(function() {
+	var imageUrl = $(this).attr('href');
+	console.log(imageUrl);
+	imageUrl.toString();
+	$(this).append( "<img src=&quot;imageUrl&quot; class="added">" );	 
+})
+	.mouseleave(function() {
+	console.log("gone");
+	//$( ".added" ).remove();
+});
+
+
+// ---
+
+$( "a" ).mouseover(function() {
+	var imageUrl = $(this).attr('href');
+	console.log(imageUrl);
+	console.log("this: " + this);
+	imageUrl.toString();
+	$(this).parent().parent().append( "<img class=added src=" + imageUrl + ">" );
+	//$(this).parent().append( "<img class=added src=http://i.imgur.com/Taq84QV.png>" );	 
+})
+	.mouseleave(function() {
+	console.log("safety: " + this);
+	//$( ".added" ).remove();
+});
